67526a31981f7bb6c9f3070801b8fab516edebd0
mu0363 <dde036@gmail.com>
2022-08-11 22:41:24 +0900

    Headerをレスポンシブに修正

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -8,8 +8,12 @@ import {
   Group,
   useMantineColorScheme,
   Container,
+  Space,
+  Drawer,
+  Stack,
 } from "@mantine/core";
 import { IconMoon, IconSun } from "@tabler/icons";
+import Link from "next/link";
 import { FC, useState } from "react";
 
 /** @package */
@@ -18,41 +22,103 @@ export const Header: FC = () => {
   // eslint-disable-next-line @typescript-eslint/unbound-method
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const isDark = colorScheme === "dark";
-  const [opened, setOpened] = useState(false);
+  const [isOpened, setIsOpened] = useState(false);
 
   return (
-    <Mantine height={70}>
-      <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
-        <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-          <Burger opened={opened} onClick={() => setOpened((o) => !o)} size="sm" ml={10} />
-        </MediaQuery>
-        <Box
-          sx={(_t) => ({
-            width: "100%",
-          })}
-        >
-          <Container size="md" p={0}>
-            <Group position="apart">
-              <Text weight={700} ml={10}>
-                John Doe Profile
+    <div>
+      <Drawer
+        opened={isOpened}
+        onClose={() => setIsOpened(false)}
+        padding="xl"
+        size="xl"
+        style={{ backgroundColor: "pink" }}
+      >
+        <Stack>
+          <Link href="/about">
+            <a>
+              <Text size={30} weight={700}>
+                About
               </Text>
-              <Group>
-                <MediaQuery smallerThan={"sm"} styles={{ display: "none" }}>
-                  <Group>
-                    <Text weight={700}>About</Text>
-                    <Text weight={700}>Blog</Text>
-                    <Text weight={700}>Portfolio</Text>
-                    <Text weight={700}>Contact</Text>
-                  </Group>
-                </MediaQuery>
-                <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30} mr={10}>
-                  {isDark ? <IconSun size={16} /> : <IconMoon size={16} />}
-                </ActionIcon>
+            </a>
+          </Link>
+          <Link href="/blog">
+            <a>
+              <Text size={30} weight={700}>
+                Blog
+              </Text>
+            </a>
+          </Link>
+          <Link href="/portfolio">
+            <a>
+              <Text size={30} weight={700}>
+                Portfolio
+              </Text>
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a>
+              <Text size={30} weight={700}>
+                Contact
+              </Text>
+            </a>
+          </Link>
+        </Stack>
+      </Drawer>
+      <Mantine height={70} style={{ position: "fixed" }}>
+        <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
+          <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+            <Burger opened={isOpened} onClick={() => setIsOpened((o) => !o)} size="sm" ml={10} />
+          </MediaQuery>
+          <Box
+            sx={(_t) => ({
+              width: "100%",
+            })}
+          >
+            <Container size="md" p={0}>
+              <Group position="apart">
+                <Link href="/">
+                  <a>
+                    <Text weight={700} ml={10}>
+                      John Doe Profile
+                    </Text>
+                  </a>
+                </Link>
+
+                <Group>
+                  <MediaQuery smallerThan={"sm"} styles={{ display: "none" }}>
+                    <Group>
+                      <Link href="/about">
+                        <a>
+                          <Text weight={700}>About</Text>
+                        </a>
+                      </Link>
+                      <Link href="/blog">
+                        <a>
+                          <Text weight={700}>Blog</Text>
+                        </a>
+                      </Link>
+                      <Link href="/portfolio">
+                        <a>
+                          <Text weight={700}>Portfolio</Text>
+                        </a>
+                      </Link>
+                      <Link href="/contact">
+                        <a>
+                          <Text weight={700}>Contact</Text>
+                        </a>
+                      </Link>
+                    </Group>
+                  </MediaQuery>
+                  <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30} mr={10}>
+                    {isDark ? <IconSun size={16} /> : <IconMoon size={16} />}
+                  </ActionIcon>
+                </Group>
               </Group>
-            </Group>
-          </Container>
-        </Box>
-      </div>
-    </Mantine>
+            </Container>
+          </Box>
+        </div>
+      </Mantine>
+      <Space h={75} />
+    </div>
   );
 };
