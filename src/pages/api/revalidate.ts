a8f724f26675225e0da7e14cce51356836c6a61d
mu0363 <dde036@gmail.com>
2022-08-24 16:24:59 +0900

    revalidateのpathを修正

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -6,8 +6,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(401).json({ message: "Invalid token" });
   }
 
+  const path = req.query.path as string;
+
   try {
-    await res.revalidate("/");
+    await res.revalidate(path);
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
