7e219448ca91accf77bfc4a0079545ba23bb7bc1
mu0363 <dde036@gmail.com>
2022-10-02 20:09:10 +0900

    GithubContentを作成

--- src/pages-components/index/GitHub/GitHub.tsx
+++ src/pages-components/index/GitHub/GitHub.tsx
@@ -1,8 +1,8 @@
-import { Space, Box, Button, Center, Stack, Text, Group, Progress } from "@mantine/core";
-import { IconGitFork, IconStar } from "@tabler/icons";
+import { Space, Box, Button, Center } from "@mantine/core";
 import type { FC } from "react";
+import type { GetRepositoriesQuery } from "src/types/generated";
 import { SectionTitle } from "src/components/SectionTitle";
-import { GetRepositoriesQuery } from "src/types/generated";
+import { GithubContent } from "src/pages-components/index/GitHub/GithubContent";
 
 type Props = {
   githubQueryData: GetRepositoriesQuery;
@@ -10,93 +10,12 @@ type Props = {
 
 /** @package */
 export const GitHub: FC<Props> = ({ githubQueryData }) => {
-  let githubNodeData;
-  if (githubQueryData.user?.pinnedItems.edges) {
-    // FIXME: non-null-assertionを解決
-    githubNodeData = githubQueryData.user.pinnedItems.edges.map((data) => data!.node);
-  }
+  const repositoryNodes = githubQueryData.user?.pinnedItems.nodes;
 
   return (
     <Box>
       <SectionTitle title="Github" />
-      {githubNodeData &&
-        githubNodeData.map((github) => {
-          type ExcludeEmptyObject<T> = T extends { id: string } ? T : never;
-          type GithubType = ExcludeEmptyObject<typeof github>;
-          const assertedGithub = github as GithubType;
-
-          const totalSize = assertedGithub.languages?.edges?.reduce((sum, edge) => {
-            return sum + (edge ? edge.size : 0);
-          }, 0);
-          const languagesInfos = assertedGithub.languages?.edges?.map((edge) => {
-            return {
-              id: edge?.node.id,
-              name: edge?.node.name,
-              percent: totalSize ? Math.round(((edge ? edge.size : 0) / totalSize) * 100) : 0,
-              color: edge?.node.color,
-            };
-          });
-
-          return (
-            <div key={assertedGithub.id}>
-              {assertedGithub && (
-                <Stack spacing={5}>
-                  <Text size="xl" weight={500}>
-                    {assertedGithub.name}
-                  </Text>
-                  <Text>{assertedGithub.description}</Text>
-
-                  <Group
-                    sx={(theme) => ({
-                      color: theme.colorScheme === "dark" ? theme.colors.dark[5] : "grey",
-                    })}
-                  >
-                    <Group>
-                      <IconStar size={18} />
-                      <Text size="sm" weight={700}>
-                        {assertedGithub.stargazerCount}
-                      </Text>
-                    </Group>
-                    <Group>
-                      <IconGitFork size={18} />
-                      <Text size="sm" weight={700}>
-                        {assertedGithub.forkCount}
-                      </Text>
-                    </Group>
-                  </Group>
-                  <Progress
-                    mt="md"
-                    size="md"
-                    radius="xl"
-                    sections={languagesInfos?.map((languagesInfo) => ({
-                      color: languagesInfo.color ? languagesInfo.color : "#FFF",
-                      value: languagesInfo.percent,
-                    }))}
-                  />
-                  <Group>
-                    {languagesInfos?.map((languagesInfo) => (
-                      <Group spacing={5} key={languagesInfo.id}>
-                        <Text size={8} color={languagesInfo.color ? languagesInfo.color : "#FFF"}>
-                          ●
-                        </Text>
-                        <Text size={12} weight={700}>
-                          {languagesInfo.name}
-                        </Text>
-                        <Text
-                          size={12}
-                          sx={(theme) => ({
-                            color: theme.colorScheme === "dark" ? theme.colors.dark[5] : "grey",
-                          })}
-                        >{`${languagesInfo.percent}%`}</Text>
-                      </Group>
-                    ))}
-                  </Group>
-                  <Space h="lg" />
-                </Stack>
-              )}
-            </div>
-          );
-        })}
+      <GithubContent repositoryNodes={repositoryNodes} />
       <Space h={20} />
       <Center>
         <Button
